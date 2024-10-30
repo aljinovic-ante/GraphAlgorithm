8ebcff8330ec77671583b18240b9c54f1b642c49
Ante <antealjinovic111@gmail.com>
2024-10-30 15:57:56 +0100

    fix bool int

--- 2. Vježba/aljinovic_ante_5.py
+++ 2. Vježba/aljinovic_ante_5.py
@@ -1,23 +1,23 @@
 # 5. Napisati iterativnu i rekurzivnu funkciju koja za listu vraća element
 # najveće brojčane vrijednosti. Vrijednosti u listi koji nisu brojevi ignorira.
 # Primjer: Za listu lst = [7, 18, 3, 'a', True, (2,3)] funkcija vraća 18.
-def rek(lst,max=None,i=0):
+def rek(lst,najveci=None,i=0):
     if i==len(lst):
-        return max
-    if isinstance(lst[i],(int,float)):
-        # print(e)
-        if max is None or max<lst[i]:
-            max=lst[i]
-    return rek(lst,max,i+1)
+        return najveci
+    if isinstance(lst[i], (int, float)) and not isinstance(lst[i], bool):
+        # print(lst[i])
+        if najveci is None or najveci<lst[i]:
+            najveci=lst[i]
+    return rek(lst,najveci,i+1)
 
 def iter(lst):
     najveci=None
     for e in lst:
-        if isinstance(e, (int, float)):
+        if isinstance(e, (int, float)) and not isinstance(e, bool):
             # print(e)
             if(najveci==None or e>najveci):
                 najveci=e
     return najveci
 
-lst = [7, 18, 31.4, 'a', True, (2,3)]
+lst = [-7, -18, -31.4, 'a', True, (2,3)]
 print(rek(lst))
